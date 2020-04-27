b5d75d5cd75228a653ac7fc734de0d5c7111b30c
firelocke <61126151+firelocke@users.noreply.github.com>
2020-04-27 11:40:28 -0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,31 @@
+    <!-- // Footer & contact info -->
+    <footer>
+        <div class="content-wrap"> <!--new content wrap div -->
+            <h2>Let's Keep in Touch!</h2>
+        
+            <!-- Social media and contact links. Add or remove any networks. -->
+            <div class="contact-info">
+                <a href="mailto:@example.com">email@example.com</a>
+                <a href="http://yourwebsite.com" target="_blank">yourwebsite.com</a>
+                <a href="#" target="_blank">Twitter</a>
+                <a href="#" target="_blank">LinkedIn</a>
+                <a href="#" target="_blank">Facebook</a>
+            </div>
+
+            <p>Copyright 2020 by Kat Hunt, all rights reserved ~ <a href="http://validator.w3.org/check?uri=referer" target="_blank" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></p>
+        </div><!-- close content-wrap div -->
+    </footer>
+    
+    <!-- JavaScript associated with the W3Schools.com Top Navigation Response Exercise -->
+    <script>
+    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+        
+        function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+    </script>
---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex,nofollow" />
+    <meta charset="utf-8">
+
+    <title>Kat Hunt | Web120</title>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@700&family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/nav.css">
+<!--    <link rel="stylesheet" href="css/form.css"> -->
+</head>
+<body>
+    <!-- // Intro -->
+    <header>
+        <div class="content-wrap"> <!--new content wrap div -->
+                <img class="profile-img" src="images/designSq.jpg" alt="laptop">
+                <img class="profile-img" src="images/authorEdge.png" alt="Kat Hunt">
+                <img class="profile-img" src="images/typewriterSq.jpg" alt="typewriter">
+            
+                <h1>Kat Hunt</h1>
+                <h2>Web120: Web Authoring II</h2>
+                
+                <nav>
+                    <ul class="topnav" id="myTopnav">
+                        <li><a href="index.php" class="selected">Welcome</a></li>
+                        <li><a href="big/index.php">Big</a></li>
+                        <li><a href="aia.php">AIA</a></li>
+                        <li><a href="flowchart.php">Flowchart</a></li>
+                        <li><a href="fp/index.php">Final Project</a></li>
+                        <li><a href="contactme.php">Contact Kat</a></li>
+                        <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+                    </ul>
+                </nav>
+
+        </div><!-- close content-wrap div -->
+   </header> 
